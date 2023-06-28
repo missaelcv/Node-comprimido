5c38a204a45f0982a0d8df88e5b1f833d8ba8621
missaelcv <missaelcv0220@gmail.com>
2023-06-28 16:21:46 -0400

    Usando el app,Rende

--- Express.js
+++ Express.js
@@ -1,12 +1,13 @@
 const express = require ('express');
+const { nextTick } = require('vue');
 const app = express();
-
 const port = 3000;
-app.use(express.static(__dirname + "/public"))
 
 //Motor de Plantillass
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
+
+app.use(express.static(__dirname + "/public"));
 
 app.get('/', (req, res) => {
     res.render("index", {titulo : "mi titulo Dinamico"})
@@ -17,7 +18,10 @@ app.get('/servicios', (req, res) => {
 })
 
 app.use((req, res, next) => {
-    res.status(404).render("404", { titulo: "Página 404" });
+    res.status(404).render("404", {
+        descripcion: "Titulo del Sitio Web"
+    })
+})
 
 app.listen(port, () => {
     console.log('El Servidor a su Servicio en el Puerto', port)
