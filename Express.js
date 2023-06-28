c2fae7707dab618d8167d4199315d5be97d34d44
missaelcv <missaelcv0220@gmail.com>
2023-06-28 15:49:08 -0400

    creaciones de Views

--- Express.js
+++ Express.js
@@ -4,21 +4,20 @@ const app = express();
 const port = 3000;
 app.use(express.static(__dirname + "/public"))
 
-app.use((req, res, next) => {
-    res.status(404).sendFile(__dirname + "/public/404.html")
-})
+//Motor de Plantillass
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
 
 app.get('/', (req, res) => {
-    res.send('Mi Respuesta desde Express');
+    res.render("index", {titulo : "mi titulo Dinamico"})
 })
 
-app.get('/Servidor', (req, res) => {
-    res.send('Esta es la Pagina Del Servidor')
+app.get('/servicios', (req, res) => {
+    res.render("servicios", {tituloServicios: "Titulo de Servicio"})
 })
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(__dirname + "/public/404.html")
-})
+    res.status(404).render("404", { titulo: "Página 404" });
 
 app.listen(port, () => {
     console.log('El Servidor a su Servicio en el Puerto', port)
