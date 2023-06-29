ad6bf1cb87a29a2f299a7809acc1a11ed3c0f4f7
missaelcv <missaelcv0220@gmail.com>
2023-06-29 00:13:59 -0400

    Respondiendo desde el Servidor

--- Express.js
+++ Express.js
@@ -7,6 +7,10 @@ app.get('/', (req, res) => {
     res.send('Mi Respuesta desde Express');
 })
 
+app.get('/Servidor', (req, res) => {
+    res.send('Esta es la Pagina Del Servidor')
+})
+
 app.listen(port, () => {
     console.log('El Servidor a su Servicio en el Puerto', port)
 })
