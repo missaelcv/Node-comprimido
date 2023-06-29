14bcc7d2d667c387e2ce09cb3b579d2dcdc3d03c
missaelcv <missaelcv0220@gmail.com>
2023-06-29 00:29:45 -0400

    Creacion de Milware

--- Express.js
+++ Express.js
@@ -2,6 +2,11 @@ const express = require ('express');
 const app = express();
 
 const port = 3000;
+app.use(express.static(__dirname + "/public"))
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(__dirname + "/public/404.html")
+})
 
 app.get('/', (req, res) => {
     res.send('Mi Respuesta desde Express');
@@ -9,6 +14,10 @@ app.get('/', (req, res) => {
 
 app.get('/Servidor', (req, res) => {
     res.send('Esta es la Pagina Del Servidor')
+})
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(__dirname + "/public/404.html")
 })
 
 app.listen(port, () => {
